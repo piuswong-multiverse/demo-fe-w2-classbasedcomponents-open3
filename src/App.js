53ebaced78ff23e0914b5f9868f543ed31a29270
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2023-06-29 11:16:05 -0500

    Change to class-based component for main app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-function App() {
-    return(
-        <div>
-            Hello! I'm in a functional React component!!
-        </div>
-    )
+class App extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return(
+            <div>
+                Hello!!! I'm in a class-based component!
+            </div>
+        )
+    }
 }
 
 export { App };
