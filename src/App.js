f7926e352e5583cc8aabdb718bcaa4a723c58cc5
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2023-06-29 11:27:30 -0500

    Fetch kanye quotes and show them

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import React from 'react';
 class App extends React.Component {
     constructor() {
         super();
+        this.state = { quote: '' };
     }
+
+    componentDidMount() {
+        fetch('https://api.kanye.rest')
+            .then( (res) => res.json() )
+            .then( (data) => {
+                console.log(data.quote);
+                this.setState( { quote: data.quote } )
+            })
+    }
+
     render() {
         return(
             <div>
-                Hello!!! I'm in a class-based component!
+                quote here: { this.state.quote }
             </div>
         )
     }
